f291721107e33484215ea957bcd1fe5cd06e36ad
muratcankirdar <muratcankirdar@gmail.com>
2023-03-14 00:20:21 +0300

    implement initial todo list in home

--- src/components/templates/home.tsx
+++ src/components/templates/home.tsx
@@ -1,13 +1,80 @@
+import { useState, useEffect } from "react";
+
 // Components
-import Example from "@/components/organisms/example"
+import Title from "@/components/atoms/Title";
+import TaskForm from "@/components/molecules/TaskForm";
 
-// Types
-type Props = {}
+import TaskItem from '@/components/molecules/TaskItem';
 
-export default function HomeTemplate({}:Props) {
+export interface IListItem {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+const HomeTemplate = () => {
+  const [text, setText] = useState<string>('');
+  const [list, setList] = useState<IListItem[]>([]);
+
+  const addItem = () => {
+    if (!text) return;
+
+    setList([...list, { text, id: generateId(), completed: false }]);
+    setText('');
+  };
+
+  const removeItem = (id: number) => {
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  const toggleItem = (id: number) => {
+    // set List item with id toggle completed
+    setList(list.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          completed: !item.completed
+        }
+      }
+      return item;
+    }));
+  };
+
+  const generateId = () => {
+    return Math.floor(Math.random() * 1000000);
+  }
+
+  useEffect(() => {
+    // save list to local storage
+    // check if list is not empty while saving
+    if (list.length > 0) {
+      localStorage.setItem('list', JSON.stringify(list));
+    }
+  }, [list]);
+
+  // load list from local storage
+  useEffect(() => {
+    const list = localStorage.getItem('list');
+
+    if (list) {
+      setList(JSON.parse(list));
+    }
+  }, []);
+
   return (
-    <div className="">
-      <Example />
+    <div>
+      <div className="p-10 flex justify-center w-full">
+        <Title label="Todo List" />
+      </div>
+
+      {/* Create TodoList organism */}
+      <TaskForm text={text} setText={setText} addItem={addItem} />
+
+      {list.map((item: IListItem) => (
+        <TaskItem item={item} removeItem={removeItem} toggleItem={toggleItem} />
+      ))}
     </div>
   )
 }
+
+export default  HomeTemplate;
