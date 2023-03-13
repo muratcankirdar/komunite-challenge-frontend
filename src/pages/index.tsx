6327e52d022eb75773f6c9cf1bdee79b38195f46
muratcankirdar <muratcankirdar@gmail.com>
2023-03-13 21:44:55 +0300

    fix typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = ({}:Props) => {
   return (
     <>
       <Head>
-        <title>Komünite Challange Front-End</title>
+        <title>Komünite Challenge Front-End</title>
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
